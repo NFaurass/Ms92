a618ee1a46244a2859c5cbfd5dd31d0bf36becf0
NFaurass <36932043+NFaurass@users.noreply.github.com>
2018-03-04 17:53:08 +0000

    Create AdShow.js

---
+++ AdShow.js
@@ -0,0 +1,3 @@
+//<![CDATA[
+(function() { var ad = document.createElement('script'); ad.type = 'text/javascript'; ad.async = true; ad.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'; var sc = document.getElementsByTagName('script')[0]; sc.parentNode.insertBefore(ad, sc); })();
+//]]>
