1d7ef458398e0f76de9e83810ddacea97bb86a90
NFaurass <36932043+NFaurass@users.noreply.github.com>
2018-03-04 18:19:38 +0000

    Update FakeVisitsBlock.js

--- FakeVisitsBlock.js
+++ FakeVisitsBlock.js
@@ -1,4 +1,4 @@
-var block = [&#39;10khits.com&#39;, &#39;alexaboostup.com&#39;, &#39;www.10khits.com&#39;, &#39;hit4hit.org&#39;, &#39;hitleap.com&#39;, &#39;otohits.com&#39;, &#39;z.com&#39;];
+var block = [&#39;10khits.com&#39;, &#39;alexaboostup.com&#39;, &#39;hit4hit.org&#39;, &#39;hitleap.com&#39;, &#39;otohits.com&#39;, &#39;z.com&#39;];
 for (var b = block.length; b--;) {
 if (document.referrer.match(block[b]))
 window.location = &quot;https://www.google.com/&quot;;
