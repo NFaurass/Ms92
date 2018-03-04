e6ee6aa2828171adaf8d534cf3a5a1af0aa52635
NFaurass <36932043+NFaurass@users.noreply.github.com>
2018-03-04 18:17:47 +0000

    Create NoneElseMahwos92.js

---
+++ NoneElseMahwos92.js
@@ -0,0 +1,10 @@
+//<![CDATA[
+$(document).ready(function()
+{
+var aa=$("#NoneElseMahwos92").val();
+if (aa == null) {
+window.location.href = "https://www.mahwos92.ml/go/facebook";
+};
+$("#NoneElseMahwos92").attr("href","https://www.mahwos92.ml/go/facebook");
+});
+//]]>
