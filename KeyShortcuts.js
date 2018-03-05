2f2c1f5bb65d03f581655711f66411426e7853bc
NFaurass <36932043+NFaurass@users.noreply.github.com>
2018-03-05 21:29:01 +0000

    Create KeyShortcuts.js

---
+++ KeyShortcuts.js
@@ -0,0 +1,18 @@
+window.addEventListener("keydown", homePage, false);
+function homePage(key){
+    if (key.keyCode == "32"){
+window.location.href = 'https://www.mahwos92.ml/';
+    }
+}
+window.addEventListener("keydown", contactPage, false);
+function contactPage(key){
+    if (key.keyCode == "69"){
+window.location.href = 'https://www.mahwos92.ml/p/contact-us.html';
+    }
+}
+window.addEventListener("keydown", topPage, false);
+function topPage(key){
+    if (key.keyCode == "16"){
+window.location.href = '#';
+    }
+}
