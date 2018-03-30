4d5b48a0047b34360a67473d9a3d13030890df3a
NFaurass <36932043+NFaurass@users.noreply.github.com>
2018-03-30 18:53:05 +0100

    Update FakeVisitsBlock.js

--- FakeVisitsBlock.js
+++ FakeVisitsBlock.js
@@ -1,4 +1,4 @@
-var block = [&#39;10khits.com&#39;, &#39;fakevisitor.com&#39;, &#39;www.babylontraffic.com&#39;, &#39;videosearch.openweb.space&#39;, &#39;geoscreenshot.com&#39;, &#39;ami.responsivedesign.is&#39;, &#39;mobiletest.me&#39;, &#39;woorank.com&#39;, &#39;alexaboostup.com&#39;, &#39;sitepricec.com&#39;, &#39;websiteoutlook.com&#39;, &#39;siterankdd.com&#39;, &#39;unitynepal.com&#39;, &#39;real.unitynepal.com&#39;, &#39;vivads.net&#39;, &#39;hit4hit.org&#39;, &#39;hitleap.com&#39;, &#39;otohits.com&#39;, &#39;z.com&#39;];
+var block = [&#39;www.10khits.com&#39;, &#39;fakevisitor.com&#39;, &#39;www.babylontraffic.com&#39;, &#39;videosearch.openweb.space&#39;, &#39;geoscreenshot.com&#39;, &#39;ami.responsivedesign.is&#39;, &#39;mobiletest.me&#39;, &#39;woorank.com&#39;, &#39;alexaboostup.com&#39;, &#39;sitepricec.com&#39;, &#39;websiteoutlook.com&#39;, &#39;siterankdd.com&#39;, &#39;unitynepal.com&#39;, &#39;real.unitynepal.com&#39;, &#39;vivads.net&#39;, &#39;hit4hit.org&#39;, &#39;hitleap.com&#39;, &#39;otohits.com&#39;, &#39;z.com&#39;];
 for (var b = block.length; b--;) {
 if (document.referrer.match(block[b]))
 window.location = &quot;https://www.google.com/&quot;;
